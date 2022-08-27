381529be80ad431ff57cc7c749fe80853d836865
Martin Varela <mvarela@quanam.com>
2022-08-27 17:45:50 -0300

    ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    a_clean = a.toLowerCase().replace(/\W/g, '')
+    reversed_a = a_clean.split("").reverse().join("");
+    return reversed_a == a_clean;
 };
 
 // Do not edit below this line
