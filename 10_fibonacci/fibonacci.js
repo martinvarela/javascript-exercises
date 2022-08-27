c70305ccaca07bb63eeb90fa277d6f7127800a1b
Martin Varela <mvarela@quanam.com>
2022-08-27 17:57:36 -0300

    ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if (member < 0) return 'OOPS';
+    if (member === 0) return 0;
+    previous = 0;
+    actual = 1;
+    for (let i=1; i < member; i++) {
+        temp = actual;
+        actual = actual + previous;
+        previous = temp;
+    }
+    return actual;
 };
 
 // Do not edit below this line
