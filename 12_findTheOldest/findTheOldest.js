99db52dae89a60f7e2ce16ad53c38dbe9138e7b4
Martin Varela <mvarela@quanam.com>
2022-08-27 18:21:24 -0300

    ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldest = false;
+    let oldest_age = 0;
+    people.forEach(person => {
+        let person_age = getPersonAge(person.yearOfBirth, person.yearOfDeath);
+        if (person_age > oldest_age) {
+            oldest_age = person_age;
+            oldest = person;
+        }
+    });
+    return oldest;
 
+    ;
+};
+
+const getPersonAge = function (birth, end) {
+    if (!end) {
+        end = new Date().getFullYear();
+    }
+    return end - birth;
 };
 
 // Do not edit below this line
